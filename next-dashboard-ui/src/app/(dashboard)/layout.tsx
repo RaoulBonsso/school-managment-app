c64df5d65113014263a6c3caa2833ee0d57741b2
goldspide <raoulbonsso@gmail.com>
2025-01-17 23:44:48 +0100

    sidebar menu elements placed

--- next-dashboard-ui/src/app/(dashboard)/layout.tsx
+++ next-dashboard-ui/src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
 
 export default function RootLayout({
   children,
@@ -5,12 +9,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <div className="h-screen flex ">
-
-    {/* LEFT */}
-    <div className="w-1/6 bg-red-50 md:w-[8%] lg:w-[16%] xl:w-[14%]">l</div>
-    {/* RIGHT */}
-    <div className="w-5/6 bg-blue-50 md:w-[92%] lg:w-[84%] xl:w-[86%]">r</div>
-   </div>
+    <div className="h-screen flex ">
+      {/* LEFT */}
+      <div className="w-1/6 bg-red-50 md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchooLama</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className="w-5/6 d:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
   );
 }
--- next-dashboard-ui/src/app/components/Menu.tsx
+++ next-dashboard-ui/src/app/components/Menu.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import data from "@/lib/menu_items_data";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='mt-4 texr-sm'>{data.map(menu =>(
+      <div className='flex flex-col gap-2' key={menu.title}> 
+      <span className='hidden lg:block text-gray-400 font-light my-4'>{menu.title}</span>
+      {menu.items.map(item =>(
+        <Link href={item.href} key={item.label} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2'>
+          <Image src={item.icon} alt='' width={20} height={20}/>
+          <span className='hidden lg:block'>{item.label}</span>
+        </Link>
+      ))}
+      </div>
+    ))}</div>
   )
 }
 

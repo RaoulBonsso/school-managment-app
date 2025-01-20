bae430d296a1e74e9f4ba6342ce9dcdff040b911
goldspide <raoulbonsso@gmail.com>
2025-01-20 13:00:05 +0100

    chart for student number implemented using recharts from google react resource

--- next-dashboard-ui/src/app/(dashboard)/admin/page.tsx
+++ next-dashboard-ui/src/app/(dashboard)/admin/page.tsx
@@ -1,25 +1,37 @@
+import CountChart from "@/app/components/CountChart";
 import UserCard from "@/app/components/UserCard";
 import React from "react";
 
 const page = () => {
-  return <div className="p-4 flex gap-4 flex-col md:flex-row">
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/*LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="parents" />
+          <UserCard type="teachers" />
+          <UserCard type="staff" />
+          <div className=""></div>
+        </div>
+        {/*MIDDLE CHARTS */}
+        <div className=" flex gap-4 flex-col lg:flex-row">
+          {/*COUNT CHARTS */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/*ATTENDANCE CHARTS */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/*BOTTOM CHARTS */}
+        <div className=""></div>
+      </div>
 
-    {/*LEFT */}
-    <div className="w-full lg:w-2/3">
-    {/* USER CARDS */}
-    <div className="flex gap-4 justify-between flex-wrap">
-      <UserCard type="student"/>
-      <UserCard type="parents"/>
-      <UserCard type="teachers"/>
-      <UserCard type="staff"/>
-
+      {/*RIGHT*/}
+      <div className="w-full lg:w-1/3">r</div>
     </div>
-
-    </div>
-
-    {/*RIGHT*/}
-    <div className="w-full lg:w-2/3">r</div>
-  </div>;
+  );
 };
 
 export default page;
